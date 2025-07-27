46457f314cf9ce9cf1e573dfa437d55eb55c93d6
hux-x <husnainiqbal577@gmail.com>
2025-07-27 19:53:56 +0500

    .

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const {blogPosts} = require('./src/data/blogs')
 module.exports = {
-  siteUrl: 'https://britishiptvservices.com',
+  siteUrl: 'https://canadianiptvservices.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   exclude: ['/admin/*'], // if needed
@@ -22,7 +22,7 @@ module.exports = {
  
 
     return blogPosts.map((post) => ({
-      loc: `https://britishiptvservices.com/blogs/${post.id}`,
+      loc: `https://canadianiptvservices.com/blogs/${post.id}`,
       lastmod: new Date(post.date).toISOString(),
       changefreq: 'daily',
       priority: 0.7,
